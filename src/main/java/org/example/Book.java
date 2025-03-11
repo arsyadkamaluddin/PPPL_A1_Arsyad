ccccd6f67d0a4fb44fcc0564b500ac56d8d27d78
d-arsya <kamaluddin.arsyad17@gmail.com>
2025-03-12 02:26:57 +0700

    finish: pertemuan_4

--- src/main/java/org/example/Book.java
+++ src/main/java/org/example/Book.java
@@ -1,7 +1,5 @@
 package org.example;
 
-import java.util.Date;
-
 public class Book {
     int id;
     String title;
--- src/test/java/org/example/LibraryHelperTest.java
+++ src/test/java/org/example/LibraryHelperTest.java
@@ -1,42 +1,50 @@
 package org.example;
 
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.List;
 
-class LibraryHelperTest {
+import static org.mockito.ArgumentMatchers.any;
+
+@ExtendWith(MockitoExtension.class)
+public class LibraryHelperTest {
+
+    @Mock
+    private LibraryService service;
+
+    @InjectMocks
+    private LibraryHelper helper;
+
     @Test
-    void  testCountBooks(){
-        LibraryService service = Mockito.mock(LibraryService.class);
+    void testCountBooks() {
         List<Book> books = new ArrayList<>();
-        books.add(new Book(1,"Buku Biologi","Guru Biologi"));
-        books.add(new Book(2,"Buku Agama","Guru Agama"));
-        LibraryHelper helper = new LibraryHelper(service);
+        books.add(new Book(1, "Intro OOP", "Bevan"));
+        books.add(new Book(2, "Pengujian PL itu mudah", "Ernest"));
         Mockito.when(service.getAllBooks()).thenReturn(books);
-        Assertions.assertEquals(2,helper.countBooks());
+        Assertions.assertEquals( 2, helper.countBooks());
     }
 
     @Test
-    void  testStoreBooksEmpty(){
-        LibraryService service = Mockito.mock(LibraryService.class);
+    void testSaveEmptyBooks() {
         List<Book> books = new ArrayList<>();
-        LibraryHelper helper = new LibraryHelper(service);
         helper.saveBooks(books);
-        Mockito.verify(service, Mockito.never()).storeBooks(books);
+        Mockito.verify(service, Mockito.never()).storeBooks(any());
     }
 
     @Test
-    void  testStoreBooksFilled(){
-        LibraryService service = Mockito.mock(LibraryService.class);
+    void testNotEmptyBooks() {
         List<Book> books = new ArrayList<>();
-        LibraryHelper helper = new LibraryHelper(service);
-        books.add(new Book(1,"Buku Biologi","Guru Biologi"));
-        books.add(new Book(2,"Buku Agama","Guru Agama"));
+        books.add(new Book(1, "Intro OOP", "Bevan"));
+        books.add(new Book(2, "Pengujian PL itu mudah", "Ernest"));
         helper.saveBooks(books);
-        Mockito.verify(service, Mockito.atMostOnce()).storeBooks(books);
+
+        Mockito.verify(service).storeBooks(books);
     }
 }
